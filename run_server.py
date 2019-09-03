ed4c917051aa9dcc0db5aed0ed92eb3f2f02aa73
Eric Winn <eng.eric.winn@gmail.com>
2019-09-04 07:25:09 +0800

    启动程序

---
+++ run_server.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author         : Eric Winn
+# @Email          : eng.eric.winn@gmail.com
+# @Time           : 19-9-3 下午9:18
+# @Version        : 1.0
+# @File           : run_server
+# @Software       : PyCharm
+
+import sys
+import subprocess
+
+if __name__ == '__main__':
+    subprocess.call('python3 jms start all', shell=True,
+                    stdin=sys.stdin, stdout=sys.stdout)
